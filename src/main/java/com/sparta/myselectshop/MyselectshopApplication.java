b3e85f8aef7a4fc3d7880285476b8786b5533826
jiwoo <1114jiwoo@gmail.com>
2024-06-16 13:48:43 +0900

    Controller 테스트

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -3,8 +3,10 @@ package com.sparta.myselectshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing
+@EnableScheduling
 @SpringBootApplication
 public class MyselectshopApplication {
 
